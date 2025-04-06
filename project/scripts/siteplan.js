2a9a459bc27f50fcaca32cf520ad9122696c6c40
d-alsar <doglasreyes290@gmail.com>
2025-04-06 15:37:07 -0600

    siteplan completed

---
+++ project/scripts/siteplan.js
@@ -0,0 +1,17 @@
+const yearElement = document.querySelector("#currentYear");
+const currentYear = new Date().getFullYear();
+
+// This creates a copyright symbol element
+const copyrightSymbol = document.createTextNode("© ");
+
+// This adds the copyright symbol and year to the span element
+yearElement.appendChild(copyrightSymbol);
+yearElement.appendChild(document.createTextNode(currentYear));
+
+
+const today = new Date();
+const dayElement = document.querySelector("#lastModified");
+
+dayElement.innerHTML = `Last modification: <span class="highlight">${new Intl.DateTimeFormat("en-US", {
+    dateStyle: "full"
+}).format(today)}</span>`;
